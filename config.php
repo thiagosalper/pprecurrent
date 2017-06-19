5a64f7ec4ef843150e3bc907f797ad74395ef605
Thiago Pereira <thiagopereira@MacBook-Pro-de-Thiago.local>
2017-06-18 21:13:07 -0300

    configs

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 // configs to paypal integration
 DEFINE("PP_USER", "thiago_salper-facilitator_api1.hotmail.com");
-DEFINE("PP_PWD", "1385381718");
-DEFINE("PP_SIGNATURE", "AiPC9BjkCyDFQXbSkoZcgqH3hpacAGAprxx6I.a4wt3FY2kMrSvBPsU6");
+DEFINE("PP_PWD", "YOUR_PASS");
+DEFINE("PP_SIGNATURE", "YOUR_KEY");
 DEFINE("PP_CANCELURL", "http://localhost/cancel.html");
-DEFINE("PP_RETURNURL", "http://localhost/testerecorrencia/paypal/createRecurringPaymentsProfile.php");
+DEFINE("PP_RETURNURL", "http://localhost/pprecurrent/paypal/createRecurringPaymentsProfile.php");
 
 // data base
 DEFINE("DB_HOST", "localhost");
